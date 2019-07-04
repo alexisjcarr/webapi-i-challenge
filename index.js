71f4dc0cf5d5697e449f2137fdb215fdd073b04d
alexisjcarr <acarr59@gmail.com>
2019-07-04 00:05:20 -0500

    Refactors from Promise syntax to async await

--- index.js
+++ index.js
@@ -10,102 +10,99 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/api/users", (req, res) => {
-  db.find()
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: "The users' information could not be retrieved."
-      });
+app.get("/api/users", async (req, res) => {
+  try {
+    const user = await db.find();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({
+      error: "The users' information could not be retrieved."
     });
-});
-
-app.post("/api/users", (req, res) => {
-  const { name, bio } = req.body;
-  if (!name || !bio) {
-    return res.status(400).send({
-      errorMessage: "Please provide name and bio for the user."
-    });
-  } else {
-    db.insert(req.body)
-      .then(user => {
-        res.status(201).json(user);
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: "There was an error while saving the user to the database."
-        });
-      });
   }
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const { id } = req.params;
-
-  db.findById(id)
-    .then(user => {
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({
-          message: "The user with the specified ID does not exist."
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: "The user information could not be retrieved."
-      });
+app.post("/api/users", async (req, res) => {
+  const { name, bio } = req.body;
+  
+  if (!name || !bio) {
+    res.status(400).send({
+      errorMessage: "Please provide name and bio for the user."
     });
+  } else {
+    try {
+      const user = await db.insert(req.body);
+      res.status(201).json(user);
+    } catch (err) {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database."
+      });
+    }
+  }
 });
 
-app.delete("/api/users/:id", (req, res) => {
+app.get("/api/users/:id", async (req, res) => {
   const { id } = req.params;
 
-  db.remove(id)
-    .then(user => {
-      if (!user) {
-        res.status(404).json({
-          message: "The user with the specified ID does not exist."
-        });
-      } else {
-        res.status(204).end();
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: "The user could not be removed."
+  try {
+    const user = await db.findById(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist."
       });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "The user information could not be retrieved."
     });
+  }
 });
 
-app.put("/api/users/:id", (req, res) => {
+app.delete("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await db.remove(id);
+    if (!user) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist."
+      });
+    } else {
+      res.status(204).end();
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "The user could not be removed."
+    });
+  }
+});
+
+app.put("/api/users/:id", async (req, res) => {
   const { id } = req.params;
   const updatedInfo = req.body;
+  const { name, bio } = updatedInfo;
 
-  if (!updatedInfo.name || !updatedInfo.bio) {
+  if (!name || !bio) {
     res.status(400).json({
       errorMessage: "Please provide name and bio for the user."
     });
   }
 
-  db.update(id, updatedInfo)
-    .then(updated => {
-      if (!updated) {
-        res.status(404).json({
-          message: "The user with the specified ID does not exist."
-        });
-      } else {
-        res.status(200).json(updated);
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: "The user information could not be modified."
+  try {
+    const updated = await db.update(id, updatedInfo);
+    if (!updated) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist."
       });
+    } else {
+      res.status(200).json(updated);
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "The user information could not be modified."
     });
+  }
 });
 
 app.listen(8080, () => {
